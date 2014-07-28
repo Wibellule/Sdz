3702f9cd2dab14ed41d38c5dd14b88f8780be6ec
Wibellule <gyome34@hotmail.com>
2014-07-28 20:38:26 +0200

    Create ArticleEditType.php
    - Mise en place de l'héritage de formulaire pour l'édition, avec suppression de l'affichage du champ date

---
+++ BlogBundle/Form/ArticleEditType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Sdz\BlogBundle\Form;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ArticleEditType extends ArticleType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // On fait appel à la méthode buildForm du parent, qui
+        // va ajouter tous les champs à $builder
+        parent::buildForm($builder, $options);
+        $builder->remove('date');
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sdz\BlogBundle\Entity\Article'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sdz_blogbundle_articleedittype';
+    }
+}
