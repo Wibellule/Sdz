2e038b5f510717c560deaf2709184c66b5485e93
Wibellule <gyome34@hotmail.com>
2014-07-28 21:00:41 +0200

    Update ArticleType.php
    - Ajout des évènements pour le formulaire afin d'ajouter ou d'enlever des champs selon des conditions en base de données

--- BlogBundle/Form/ArticleType.php
+++ BlogBundle/Form/ArticleType.php
@@ -19,7 +19,6 @@ class ArticleType extends AbstractType
             ->add('titre',      'text')
             ->add('contenu',    'textarea')
             ->add('auteur',     'text')
-            ->add('publication','checkbox', array('required' => false))
             ->add('image',      new ImageType()) // Formulaire imbriqué
             ->add('categories',  'entity',
                 array(
@@ -29,6 +28,39 @@ class ArticleType extends AbstractType
                 )
             )
         ;
+
+        $factory = $builder->getFormFactory();
+
+        // On ajoute une fonction qui va écouter l'évènement PRE_SET_DATA
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA, // Ici, on définit l'évènement qui nous intéresse
+            function(FormEvents $event) use ($factory) // Ici on définit une fonction qui sera executé lors de l'évent
+            {
+                // On récupère l'objet
+                $article = $event->getData();
+                // Cette condition est importante
+                if( null === $article )
+                {
+                    return; // On sort de la fonction lorsque $article vaut null
+                }
+                // Si l'article n'est pas encore publié, on ajoute le champ publication
+                if( false === $article->getPublication() )
+                {
+                    // On récupère le formulaire et on ajoute le champ publication
+                    $event->getForm()->add(
+                        $factory->createNamed(
+                            'publication',
+                            'checkbox',
+                            null,
+                            array('required' => false)
+                        )
+                    );
+                }else{
+                    // Sinon on le supprime
+                    $event->getForm()->remove('publication');
+                }
+            }
+        );
     }
     
     /**
