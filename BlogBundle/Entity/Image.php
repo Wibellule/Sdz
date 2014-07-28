c9839dcb2886264f79eb183799299c31d94b5515
Wibellule <gyome34@hotmail.com>
2014-07-28 22:02:53 +0200

    Update Image.php
    - Mise en place des évènements/callbacks pour l'upload des fichiers

--- BlogBundle/Entity/Image.php
+++ BlogBundle/Entity/Image.php
@@ -1,5 +1,7 @@
 <?php
 
+// src/Sdz/BlogBundle/Entity/Image.php
+
 namespace Sdz\BlogBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -9,6 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @ORM\Table(name="sdz_image")
  * @ORM\Entity(repositoryClass="Sdz\BlogBundle\Entity\ImageRepository")
+ * @ORM\HasLifecycleCallbacks
  */
 class Image
 {
@@ -40,6 +43,11 @@ class Image
      * @ORM\Column(name="alt", type="string", length=255)
      */
     private $alt;
+
+    private $file;
+
+    // On ajoute cet attribut pour y stocker le nom du fichier
+    private $tempFileName;
 
     /**
      * Get id
@@ -95,6 +103,87 @@ class Image
     public function getAlt()
     {
         return $this->alt;
+    }
+
+    // On modifiera le setter de File, pour prendre en compte
+    // l'upload d'un fichier lorsqu'il en existe déjà un autre
+    public function setFile($file)
+    {
+        $this->file = $file;
+
+        // On vérifie si on avait déjà un fichier pour cette entité
+        if( null !== $this->url )
+        {
+            // On sauvegarde l'extension du fichier pour le supprimer plus tard
+            $this->tempFileName = $this->url;
+
+            // On réinitialise les valeurs des attributs url et alt
+            $this->url = null;
+            $this->alt = null;
+        }
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        // Si jamais il n'y a pas de fichier ( champ facultatif )
+        if( null === $this->file)
+        {
+            return;
+        }
+
+        // Le nom du fichier est son id, on doit juste stocker également son extension
+        // Pour faire propre, on devrait renommer cet attribut en "extension" plutôt que "url"
+        $this->url = $this->file->guessFileExtension();
+
+        // Et on génère l'attribut alt de la balise <img />, à la
+        // valeur du nom du fichier sur le PC de l'internaute
+        $this->alt = $this->file->getClientOriginalName();
+    }
+
+    /**
+     * @ORM\PostPersist
+     * @ORM\PostUpdate
+     */
+    public function upload()
+    {
+        // Si jamais il n'y a pas de fichier ( champ facultatif )
+        if( null === $this->file )
+        {
+            return;
+        }
+
+        // Si on avait un ancien fichier, on le supprime
+        if( null !== $this->tempFileName )
+        {
+            $oldFile = $this->getUploadRootDir().'/'.$this->id.'.'.$this->tempFileName;
+            if(file_exists($oldFile))
+            {
+                unlink($oldFile);
+            }
+        }
+
+        // On déplace le fichier envoyé dans le repertoire de notre choix
+        $this->file->move(
+            $this->getUploadRootDir(), // Le répertoire de destination
+            $this->id.'.'.$this->url // Le nom du fichier à créer ici "id.extension"
+        );
+
+    }
+
+    public function getUploadDir()
+    {
+        // On retourne le chemin relatif vers l'image pour un navigateur
+        return 'uploads/img';
+    }
+
+    protected function getUploadRootDir()
+    {
+        // On retourne le chemin relatif vers l'image pour notre code PHP
+        return __DIR__.'/../../../../web'.$this->getUploadDir();
     }
 
 
