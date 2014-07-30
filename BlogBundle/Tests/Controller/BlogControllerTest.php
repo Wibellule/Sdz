729f9c36bffa28733103f26e45cfdcd2bd066c02
Wibellule <Wibellule@users.noreply.github.com>
2014-07-30 21:57:56 +0200

    Create BlogControllerTest.php
    - Mise en place d'un début de fonction pour des tests unitaires

---
+++ BlogBundle/Tests/Controller/BlogControllerTest.php
@@ -0,0 +1,15 @@
+<?php 
+
+// src/Sdz/BlogBundle/Tests/Controller/BlogBundleTest.php
+
+namespace Sdz\BlogBundle\Tests\Controller;
+
+use Sdz\BlogBundle\Controller\BlogController;
+
+class BlogControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIndexAction()
+    {
+    
+    }
+}
