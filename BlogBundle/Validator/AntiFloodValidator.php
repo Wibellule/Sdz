166ff4f1a7697b98cb721528fe1ebbd1e7d1ec41
Wibellule <Wibellule@users.noreply.github.com>
2014-08-03 19:14:56 +0200

    Create AntiFloodValidator.php
    - Ajout de la classe AntiFloodValidator qui va traiter le formulaire et valider ou non

---
+++ BlogBundle/Validator/AntiFloodValidator.php
@@ -0,0 +1,22 @@
+<?php
+
+// src/Sdz/BlogBundle/Validator/AntiFloodValidator.php
+
+namespace Sdz\BlogBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class AntiFloodValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        // Pour l'instant, on considère comme flood tout message de moins de 3 caractères
+        if(strlen($value) < 3)
+        {
+            // C'est cette ligne qui déclenche l'erreur pour le formulaire
+            // Avec en argument le message
+            $this->context->addViolation($constraint->message);
+        }
+    }
+}
