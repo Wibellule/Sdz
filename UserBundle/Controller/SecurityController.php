eb41299c4ab271d599ebb01fa6ff75fa6ac2d6eb
Wibellule <gyome34>
2014-08-07 20:31:00 +0200

    Push Sdz/UserBundle
    - Ajout du UserBundle

---
+++ UserBundle/Controller/SecurityController.php
@@ -0,0 +1,16 @@
+<?php
+
+# src/Sdz/UserBundle/Controller/SecurityController.php
+
+namespace Sdz\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+        
+    }
+}
