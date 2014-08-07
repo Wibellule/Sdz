01dc09d1339fdf28c53e618ae06863e6a59e196d
Wibellule <Wibellule@users.noreply.github.com>
2014-08-07 20:55:42 +0200

    Update SecurityController.php
    - Modification SecurityController

--- UserBundle/Controller/SecurityController.php
+++ UserBundle/Controller/SecurityController.php
@@ -5,12 +5,37 @@
 namespace Sdz\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContext;
 
 class SecurityController extends Controller
 {
     public function loginAction()
     {
-        
+        // Si le visiteur est déjà authentifié, on le redirige vers l'accueil
+        if($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+        {
+            return $this->redirect($this->generateUrl('sdzblog_accueil'));
+        }
+
+        $request = $this->getRequest();
+        $session = $this->getSession();
+
+        // On vérifie s'il y a des erreurs d'une précédente soumission du formulaire
+        if($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR))
+        {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        }else{
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render(
+            'SdzUserBundle:Security:login.html.twig',
+            array(
+                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                'error'         => $error
+            )
+        );
     }
 }
