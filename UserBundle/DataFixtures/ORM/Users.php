ddbbdb5e4809a9f9cbe9d634b8f87d16666a9374
Wibellule <Wibellule@users.noreply.github.com>
2014-08-10 00:21:27 +0200

    Update Users.php
    - RAZ fichier DataFixtures

--- UserBundle/DataFixtures/ORM/Users.php
+++ UserBundle/DataFixtures/ORM/Users.php
@@ -1,32 +1,18 @@
 <?php
-// src/Sdz/BlogBundle/DataFixtures/ORM/Categories.php
+// src/Sdz/BlogBundle/DataFixtures/ORM/Users.php
 
 namespace Sdz\BlogBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Sdz\BlogBundle\Entity\Categorie;
+use Sdz\UserBundle\Entity\User;
 
-class Categories implements FixtureInterface
+class Users implements FixtureInterface
 {
     //Dans l'argument de la méthode load, l'objet $manager est l'EntityManager
 
     public function load(ObjectManager $manager)
     {
-        //Liste des noms de catégories à ajouter
-        $noms = array('Symfony2','Doctrine2','Tutoriel','Evenement');
-
-        foreach($noms as $i => $nom)
-        {
-            //On crée la catégorie
-            $liste_categories[$i] = new Categorie();
-            $liste_categories[$i]->setNom($nom);
-
-            //On le persiste
-            $manager->persist($liste_categories[$i]);
-        }
-
-        //On déclence le flush
-        $manager->flush();
+        
     }
 }
