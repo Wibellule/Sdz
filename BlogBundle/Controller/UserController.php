3342c60c29493f3c22f1d60cb6392384f31c429a
Wibellule <Wibellule@users.noreply.github.com>
2014-08-19 13:41:12 +0200

    Update UserController.php

--- BlogBundle/Controller/UserController.php
+++ BlogBundle/Controller/UserController.php
@@ -35,79 +35,14 @@ class UserController extends Controller
 
     }
 
-    public function modifierAction(Article $article)
+    public function modifierAction(User $user)
     {
-        // On construit le form avec cette instance d'article
-        $form = $this->createForm(new ArticleEditType(), $article);
-
-        // On récupère la requête
-        $request = $this->get('Request');
-
-        // On vérifie qu'elle est de type POST
-        if($request->getMethod() == 'POST')
-        {
-            // On fait le lien requête <-> formulaire
-            // A partir de maintenant, la variable $article contient
-            // Les valeurs entrées dans le formulaire par le visiteur
-            $form->submit($request);
-
-            if($form->isValid())
-            {
-                // On enregistre notre objet $article dans la base de données
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($article);
-                $em->flush();
-
-                // On définit un message flash
-                $this->get('session')->getFlashBag()->add('info','Article bien modifié');
-
-                // On redirige vers la page de visualisation de l'article nouvellement crée
-                return $this->redirect(
-                    $this->generateUrl(
-                        'sdzblog_voir', array('slug' => $article->getSlug())
-                    )
-                );
-            }
-        }
-
-        return $this->render('SdzBlogBundle:Blog:modifier.html.twig', array(
-            'article' => $article,
-            'form'    => $form->createView()
-        ));
+        
     }
 
-    public function supprimerAction(Article $article)
+    public function supprimerAction(User $user)
     {
-        // On crée un formulaire vide, qui ne contiendra que le champ CSRF
-        // Cela permet de protéger la suppression d'article contre cette faille
-        $form = $this->createFormBuilder()->getForm();
-
-        $request = $this->get('Request');
-
-        if ($request->getMethod() == 'POST') {
-
-            $form->submit($request);
-            if($form->isValid())
-            {
-                // On supprime l'article
-                $em = $this->getDoctrine()->getManager();
-                $em->remove($article);
-                $em->flush();
-
-                // Si la requête est en POST, on supprimera l'article
-                $this->get('session')->getFlashBag()->add('info', 'Article bien supprimé');
-
-                // Puis on redirige vers l'accueil
-                return $this->redirect( $this->generateUrl('sdzblog_accueil') );
-
-            }
-        }
-
-        // Si la requête est en GET, on affiche une page de confirmation avant de supprimer
-        return $this->render('SdzBlogBundle:Blog:supprimer.html.twig', array(
-            'article' => $article,
-            'form'    => $form->createView()
-        ));
+        
     }
 
     public function menuAction($nombre)
